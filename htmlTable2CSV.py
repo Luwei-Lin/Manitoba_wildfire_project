a9d7f8f1aea3d25fb13689316c81358aa644499c
Luwei-Lin <luwei2@ualberta.ca>
2021-10-27 20:25:58 -0500

    import

--- htmlTable2CSV.py
+++ htmlTable2CSV.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("this is testing from local")
+import os
+import sys
+import pandas as pd
+from bs4 import BeautifulSoup
+
+path = "https://www.gov.mb.ca/conservation_fire/Wx-Report/2021/20211025-obs-wxrep.html"
+
+data = []
+
+
