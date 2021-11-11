e32cfe6aa70171d228105380aecde432a97c78f5
Luwei-Lin <luwei2@ualberta.ca>
2021-11-11 09:51:01 -0600

    Linear Regression Model version 1

--- linearRegression.py
+++ linearRegression.py
@@ -1,87 +1,10 @@
 #Credits to @Asad Mahmood https://towardsdatascience.com/linear-regression-with-pytorch-eb6dedead817 
-
-
 #import libraries
 import numpy as np
 import torch
 from torch.autograd import Variable
 import pandas as pd
-
-
-#creating the training data
-
-dataSet = pd.read_csv('summary_weather_and_burned_area.csv')
-
-df_x = dataSet[['DATE', 'TEMP', 'RH', 'WS', 'FFMC', 'DMC', 'DC', \
-    'ISI', 'BUI', 'FWI', 'DSR', 'RAIN_x', 'HECTARES(yesterday)']]
-df_y = dataSet[['HECTARES']]
-
-x_train = df_x.sample(n = 1000)
-x_test = df_x.sample(n = 500)
-
-y_train = df_y.sample(n = 1000)
-y_test = df_y.sample(n = 500)
-
-
-dataList = []
-
-date = dataSet['DATE'].tolist()
-temp = dataSet['TEMP'].tolist()
-rh = dataSet['RH'].tolist()
-windSpeed = dataSet['WS'].tolist()
-ffmc = dataSet['FFMC'].tolist()
-dmc = dataSet['DMC'].tolist()
-dc = dataSet['DC'].tolist()
-isi = dataSet['ISI'].tolist()
-bui = dataSet['BUI'].tolist()
-fwi = dataSet['FWI'].tolist()
-dsr = dataSet['DSR'].tolist()
-rainX = dataSet['RAIN_x'].tolist()
-rainY = dataSet['RAIN_y'].tolist()
-burnedToday = dataSet['HECTARES'].tolist()
-burnedYesterday = dataSet['HECTARES(yesterday)'].tolist()
-
-
-dataList.append(date)
-dataList.append(temp)
-dataList.append(rh)
-dataList.append(windSpeed)
-dataList.append(ffmc)
-dataList.append(dmc)
-dataList.append(dc)
-dataList.append(isi)
-dataList.append(bui)
-dataList.append(fwi)
-dataList.append(dsr)
-dataList.append(rainX)
-dataList.append(rainY)
-dataList.append(burnedToday)
-
-
-#create training data
-x_values = []
-y_values = []
-i = 0
-
-
-#load the data into the matrix
-while i < len(date):
-	dataOfADay = [];
-	for n in range(14): #we have 14 types of data
-		#data of every day is collected into one list
-		dataOfADay.append(dataList[n])
-
-	x_values.append(dataOfADay)
-	y_values.append(burnedYesterday[i])
-	i = i + 1
-
-x_train = np.array(x_values, dtype=np.float32)
-x_train = x_train.reshape(-1, 1)
-
-y_train = np.array(y_values, dtype=np.float32)
-y_train = y_train.reshape(-1, 1)
-
-
+import matplotlib.pyplot as plt
 
 class linearRegression(torch.nn.Module):
     def __init__(self, inputSize, outputSize):
@@ -92,18 +15,39 @@ class linearRegression(torch.nn.Module):
         out = self.linear(x)
         return out
 
+#creating the training dat
+dataSet = pd.read_csv('summary_weather_and_burned_area.csv')
+df_x = dataSet[['TEMP', 'RH', 'WS', 'FFMC', 'DMC', 'DC', 'ISI', 'BUI', 'FWI', 'DSR', 'RAIN_x', 'HECTARES(yesterday)']]
+df_y = dataSet[['HECTARES']]
 
+#2017-2020 train
+    #x_train
+x_train = df_x.loc[0:1000]
+    #y_train
+y_train = df_y.loc[0:1000]
 
-inputDim = 14        # takes variable 'x' 
-outputDim = 1       # takes variable 'y'
-learningRate = 0.01 
+#2021 test
+    #x_test
+x_test = df_x.loc[1001:]
+    #y_test
+y_test = df_y.loc[1001:]
+# Last Step. x_test put in the "model" and compare with y_test
+
+inputDim = 12        # takes variable 'x' 
+outputDim = 1    # takes variable 'y'
+learningRate = 0.01
 epochs = 100
+
+x_train = np.array(x_train, dtype=np.float32).reshape(-1, inputDim)
+y_train = np.array(y_train, dtype=np.float32).reshape(-1, outputDim)
+
+x_test = np.array(x_test, dtype=np.float32).reshape(-1, inputDim)
+y_test = np.array(y_test, dtype=np.float32).reshape(-1, outputDim)
 
 model = linearRegression(inputDim, outputDim)
 
 criterion = torch.nn.MSELoss() 
 optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
-
 
 for epoch in range(epochs):
     # Converting inputs and labels to Variable
