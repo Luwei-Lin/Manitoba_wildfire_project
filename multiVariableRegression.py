5d0a5bc9a3178f376fc53d4687ee82e229e667c9
Luwei-Lin <luwei2@ualberta.ca>
2021-11-11 14:10:46 -0600

    update multi variables regression model

---
+++ multiVariableRegression.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import torch
+import torch.optim as optim
+
+df = pd.read_csv("summary_weather_and_burned_area.csv")
+df_x = df[['TEMP', 'RH', 'WS', 'FFMC', 'DMC', 'DC', 'ISI', 'BUI', 'FWI', 'DSR', 'RAIN_x', 'HECTARES(yesterday)']]
+df_y = df[['HECTARES']]
+
+x_train = df_x.loc[0:1000]
+y_train = df_y.loc[0:1000]
+
+x_test = df_x.loc[1001:]
+y_test = df_y.loc[1001:]
+
+x_train = torch.tensor(x_train.values, dtype=torch.float).t()
+y_train = torch.tensor(y_train.values, dtype=torch.float).t()
+
+x_test = torch.tensor(x_test.values, dtype=torch.float).t()
+y_test = torch.tensor(y_test.values, dtype=torch.float).t()
+
+input_dim = 12
+
+#We define the trainable parameters A and b
+
+A = torch.randn((1, input_dim), requires_grad=True) #[a1, a2, a3...an]
+b = torch.randn(1, requires_grad=True) #constant
+#Matrix multiplication
+def model(x_input):
+    return A.mm(x_input) + b
+
+def loss(y_predicted, y_target):
+    return ((y_predicted - y_target)**2).sum()
+
+#Main optimization loop
+#scenorio1
+def trainModel(epochs, learningRate):
+    optimizer = optim.Adam([A, b], lr=learningRate)
+    for epoch in range(epochs):
+
+        optimizer.zero_grad()
+
+        y_predicted = model(x_train)
+        current_loss = loss(y_predicted, y_train)
+
+        current_loss.backward()
+        optimizer.step()
+        #print(f"epoch = {epoch}, loss = {current_loss}, A = {A.detach().numpy()}, b = {b.item()}")
+
+def main():
+    #Training the model
+    learningRate = 0.001
+    #Implenment Adam algorithm
+    for num in range(5000, 105000, 5000):
+        epochs = num
+        trainModel(epochs, learningRate)
+        y_predicted = model(x_test)
+        test_loss = loss(y_predicted, y_test)
+        print("The loss of the test set: {} of {} times training and the learning rate is {}".format(test_loss, epochs, learningRate))
+
+main()
