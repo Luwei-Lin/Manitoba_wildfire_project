c591d281fcd3096c918080b15bb0782d31d795bb
Haozhi Wang <jimmywhz6@gmail.com>
2021-11-17 18:18:16 -0500

    Initial commit of LSTM

---
+++ LSTM.py
@@ -0,0 +1,23 @@
+#LSTM, credits to @Gabriel Loye
+
+
+#import the libraries
+import torch
+import torch.nn as nn
+
+
+input_dim = 12
+hidden_dim = 1
+n_layer = 1
+
+lstm_layer = nn.LSTM(input_dim, hidden_dim, n_layers, batch_first=True)
+
+batch_size = 1
+seq_len = 1
+
+inp = torch.randn(batch_size, seq_len, input_dim)
+hidden_state = torch.randn(n_layer, batch_size, hidden_dim)
+cell_state = torch.randn(n_layer, batch_size, hidden_dim)
+hidden = (hidden_state, cell_state)
+
+
