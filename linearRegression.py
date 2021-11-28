dc4ca805638aff43fa5a5291047eb5b999257825
Luwei-Lin <luwei2@ualberta.ca>
2021-11-27 21:28:42 -0600

    Update presentation ppt

--- linearRegression.py
+++ linearRegression.py
@@ -19,78 +19,80 @@ class linearRegression(torch.nn.Module):
 dataSet = pd.read_csv('summary_weather_and_burned_area.csv')
 featuresList = ['TEMP', 'RH', 'WS', 'FFMC', 'DMC', 'DC', 'ISI', 'BUI', 'FWI', 'DSR', 'RAIN_x', 'HECTARES(yesterday)']
 #df_x = dataSet[['TEMP', 'RH', 'WS', 'FFMC', 'DMC', 'DC', 'ISI', 'BUI', 'FWI', 'DSR', 'RAIN_x', 'HECTARES(yesterday)']]
+for feature in featuresList:
+#df_x = dataSet[['DSR', 'ISI', 'RAIN_x']]
+    df_y = dataSet[['HECTARES']]
+    df_x = dataSet[[feature]]
+    #2017-2020 train
+        #x_train
+    x_train = df_x.loc[0:1000]
+        #y_train
+    y_train = df_y.loc[0:1000]
 
-df_x = dataSet[['DSR', 'ISI', 'RAIN_x']]
-df_y = dataSet[['HECTARES']]
+    #2021 test
+        #x_test
+    x_test = df_x.loc[1001:]
+        #y_test
+    y_test = df_y.loc[1001:]
+    # Last Step. x_test put in the "model" and compare with y_test
 
-#2017-2020 train
-    #x_train
-x_train = df_x.loc[0:1000]
-    #y_train
-y_train = df_y.loc[0:1000]
+    inputDim = 1        # takes variable 'x' 
+    outputDim = 1    # takes variable 'y'
+    learningRate = 0.01
+    epochs = 100
 
-#2021 test
-    #x_test
-x_test = df_x.loc[1001:]
-    #y_test
-y_test = df_y.loc[1001:]
-# Last Step. x_test put in the "model" and compare with y_test
+    x_train = np.array(x_train, dtype=np.float32)#.reshape(-1, inputDim)
+    y_train = np.array(y_train, dtype=np.float32)#.reshape(-1, outputDim)
+    #x_train = np.transpose(x_train)
+    #y_train = np.transpose(y_train)
 
-inputDim = 3        # takes variable 'x' 
-outputDim = 1    # takes variable 'y'
-learningRate = 0.01
-epochs = 100
+    x_test = np.array(x_test, dtype=np.float32).reshape(-1, inputDim)
+    y_test = np.array(y_test, dtype=np.float32).reshape(-1, outputDim)
 
-x_train = np.array(x_train, dtype=np.float32)#.reshape(-1, inputDim)
-y_train = np.array(y_train, dtype=np.float32)#.reshape(-1, outputDim)
-#x_train = np.transpose(x_train)
-#y_train = np.transpose(y_train)
+    model = linearRegression(inputDim, outputDim)
 
-x_test = np.array(x_test, dtype=np.float32).reshape(-1, inputDim)
-y_test = np.array(y_test, dtype=np.float32).reshape(-1, outputDim)
+    criterion = torch.nn.MSELoss() 
+    optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
 
-model = linearRegression(inputDim, outputDim)
+    for epoch in range(epochs):
+        # Converting inputs and labels to Variable
+        if torch.cuda.is_available():
+            inputs = Variable(torch.from_numpy(x_train).cuda())
+            labels = Variable(torch.from_numpy(y_train).cuda())
+        else:
+            inputs = Variable(torch.from_numpy(x_train))
+            labels = Variable(torch.from_numpy(y_train))
 
-criterion = torch.nn.MSELoss() 
-optimizer = torch.optim.SGD(model.parameters(), lr=learningRate)
+        # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
+        optimizer.zero_grad()
 
-for epoch in range(epochs):
-    # Converting inputs and labels to Variable
-    if torch.cuda.is_available():
-        inputs = Variable(torch.from_numpy(x_train).cuda())
-        labels = Variable(torch.from_numpy(y_train).cuda())
-    else:
-        inputs = Variable(torch.from_numpy(x_train))
-        labels = Variable(torch.from_numpy(y_train))
+        # get output from the model, given the inputs
+        outputs = model(inputs)
 
-    # Clear gradient buffers because we don't want any gradient from previous epoch to carry forward, dont want to cummulate gradients
-    optimizer.zero_grad()
+        # get loss for the predicted output
+        loss = criterion(outputs, labels)
+        print(loss)
+        # get gradients w.r.t to parameters
+        loss.backward()
 
-    # get output from the model, given the inputs
-    outputs = model(inputs)
+        # update parameters
+        optimizer.step()
 
-    # get loss for the predicted output
-    loss = criterion(outputs, labels)
-    print(loss)
-    # get gradients w.r.t to parameters
-    loss.backward()
+        print('epoch {}, loss {}'.format(epoch, loss.item()))
 
-    # update parameters
-    optimizer.step()
-
-    print('epoch {}, loss {}'.format(epoch, loss.item()))
-
-with torch.no_grad(): # we don't need gradients in the testing phase
-    if torch.cuda.is_available():
-        predicted = model(Variable(torch.from_numpy(x_train).cuda())).cpu().data.numpy()
-    else:
-        predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
-    print(predicted)
-    plt.clf()
-    plt.plot(x_train, y_train, 'go', label='True data', alpha=0.5)
-    plt.plot(x_train, predicted, '--', label='Predictions', alpha=0.5)
-    plt.legend(loc='best')
-    plt.show()
+    with torch.no_grad(): # we don't need gradients in the testing phase
+        if torch.cuda.is_available():
+            predicted = model(Variable(torch.from_numpy(x_train).cuda())).cpu().data.numpy()
+        else:
+            predicted = model(Variable(torch.from_numpy(x_train))).data.numpy()
+        print(predicted)
+        plt.clf()
+        plt.xlabel(feature)
+        plt.ylabel('Burned Area')
+        plt.plot(x_train, y_train, 'go', label='True data', alpha=0.5)
+        plt.plot(x_train, predicted, '--', label='Predictions', alpha=0.5)
+        plt.legend(loc='best')
+        plt.show()
 
 
 
